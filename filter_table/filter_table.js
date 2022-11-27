412a54633bbd7b9e178ec1ae28aa76aaf23eb148
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-11-26 21:31:50 -0400

    Table filter refactoring.

--- filter_table/filter_table.js
+++ filter_table/filter_table.js
@@ -4,13 +4,33 @@ function filterTable(table, filterText) {
 	for (let row = 0; row < trList.length; row++) {
 		const tr = trList[row];
 		const tdList = tr.querySelectorAll("td");
-		tr.style.display = "none";
+		let show = false;
 		for (let col = 0; col < tdList.length; col++) {
 			const tdText = tdList[col].innerText.toUpperCase();
 			if (tdText.indexOf(filterText) >= 0){
-				tr.style.display = "";
+				show = true;
 				break;
 			}
 		}
+		tr.style.display = show ? null : "none";
 	}
+}
+
+function ScheduleSingleTask(timeout, taskId, task) {
+
+	if (!ScheduleSingleTask.timeoutMap) {
+		ScheduleSingleTask.timeoutMap = new Map();
+	}
+	
+	if (ScheduleSingleTask.timeoutMap.has(taskId)) {
+		clearTimeout(ScheduleSingleTask.timeoutMap.get(taskId));
+		ScheduleSingleTask.timeoutMap.delete(taskId);
+	}
+	
+	const fn = () => {
+		ScheduleSingleTask.timeoutMap.delete(taskId);
+		task();
+	}
+	
+	ScheduleSingleTask.timeoutMap.set(taskId, setTimeout(fn, timeout));
 }
